d30b257ca0a560cf421b388eff3a299fb65e0337
jessicametro <jessica.metro@gmail.com>
2012-04-12 03:55:03 -0500

    first commit

---
+++ site.js
@@ -0,0 +1,124 @@
+
+
+$(function() {
+
+    //addInboundClassToLinks();
+    makeTopbarFade();
+
+    if (shouldDoScrollingStuff()) {
+        watchForScrolling();
+        watchForHashChange();
+        updateSelected();
+    }
+
+});
+
+
+//never called.
+function addInboundClassToLinks() {
+    $('a').each(function() {
+       var that = $(this);
+       var href = that.attr('href');
+       if(href.search('/') == -1 && href.search('#') == -1) {      
+          that.addClass('inbound');
+       }
+    });
+}
+
+function makeTopbarFade() {
+    if ($("#mainimage").length == 0) return; //nothing to do here
+    $("a.inbound").click(function(e) {
+        e.preventDefault();
+        href = $(this).attr('href');
+        $("html, body").animate({ scrollTop: 0 }, 300);
+        $("body").css("height", $("body").height());
+        $('#mainimage').animate( { 'height': '0px' }, 500, function() {
+            window.location = href;
+        });
+    });
+    $("a.home").click(function(e) {
+        e.preventDefault();
+        href = $(this).attr('href');
+        var mainimage = $("#mainimage");
+        mainimage.show();
+        $('#slider').nivoSlider({
+    		effect: "fade",
+    		controlNav: false,
+    		captionOpacity: 0.9,
+    	});
+    	var height = mainimage.height();
+        mainimage.css("height", 0);
+        $("html, body").animate({ scrollTop: 0 }, 300);
+        $("body").css("height", $("body").height());
+        mainimage.animate( { 'height': height }, 500, function() {
+            window.location = href;
+        });
+    });
+
+}
+
+function shouldDoScrollingStuff() {
+    if ($("#scroller").length == 0) return false;
+    return true;
+}
+
+function watchForHashChange() {
+    $(window).bind('hashchange', function() {
+        var url = $.param.fragment();
+        console.log("URL CHANGED: "+url);
+    });
+}
+
+function watchForScrolling() {
+    var scrolleritem = $("#scroller");
+    var scrolly = scrolleritem.offset().top;
+    $(document).scroll(function() {
+        var windowy = $(window).scrollTop();
+        if (scrolly >= windowy) {
+            hideHoverScroller();
+        } else {
+            showHoverScroller();
+        }
+        updateSelected();
+    });
+}
+
+function updateSelected() {
+    $(".scrollwatcher").each(function(index, Element) {
+        var name = $(this).attr('id');
+        var itemy = $(this).offset().top;
+        var height = $(this).height();
+        var scrolly = $(window).scrollTop();
+        scrolly  = scrolly + 100; //give us some buffer;
+        if ((itemy+height) > scrolly && itemy < scrolly) {
+            selectInScrollbar(name);
+        } else {
+            unselectInScrollbar(name);
+        }
+    });
+}
+function selectInScrollbar(name) {
+    $("a.scrolllink").each(function(index, element) {
+        var linkname = $(this).attr("href").replace("#", "");
+        if (linkname == name) {
+            $(this).addClass("selected");
+        }
+    });
+}
+function unselectInScrollbar(name) {
+    $("a.scrolllink").each(function(index, element) {
+        var linkname = $(this).attr("href").replace("#", "");
+        if (linkname == name) {
+            $(this).removeClass("selected");
+        }
+    });
+}
+function showHoverScroller() {
+    $("#scroller-fixed").show();
+    $("#scroller").hide();
+}
+
+function hideHoverScroller() {
+    $("#scroller-fixed").hide();
+    $("#scroller").show();
+}
